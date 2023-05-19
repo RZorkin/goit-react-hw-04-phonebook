f04309986fb1cf57972e91b42e1081a9024355ae
RZorkin <zorkin81@gmail.com>
2023-05-19 20:34:01 +0300

    Add project files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,67 @@
+import React, { useState, useEffect } from 'react';
+import { nanoid } from 'nanoid';
+
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
+import { Section } from './Section/Section';
+
+const initialState = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
+
 export const App = () => {
+  const [contacts, setContacts] = useState(
+    () => JSON.parse(localStorage.getItem('contacts')) ?? initialState
+  );
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addContact = ({ name, number }) => {
+    const contact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+
+    const includeName = contacts.find(user => user.name === contact.name);
+    if (includeName) {
+      alert(`${contact.name} is already in contacs`);
+      return;
+    }
+    setContacts(prevContacts => [contact, ...prevContacts]);
+  };
+
+  const deleteContact = id => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== id)
+    );
+  };
+
+  const changeInput = e => {
+    const { value } = e.target;
+    setFilter(value);
+  };
+
+  const filterContacts = contacts.filter(contact => {
+    return contact.name.toLowerCase().includes(filter.toLowerCase());
+  });
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Section title="Phonebook">
+        <ContactForm onSubmit={addContact} />
+      </Section>
+      <Section title="Contacts">
+        <Filter name={'filter'} changeInput={changeInput} />
+        <ContactList contacts={filterContacts} deleteContact={deleteContact} />
+      </Section>
+    </>
   );
 };
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+import css from './ContactList.module.css';
+
+export function ContactList({ contacts, deleteContact }) {
+  return (
+    <ul className={css.list}>
+      {contacts.map(({ id, name, number }) => {
+        return (
+          <>
+            <li className={css.item}>
+              <p>
+                {name}:{number}
+              </p>
+              <button
+                className={css.button}
+                type="button"
+                onClick={() => deleteContact(id)}
+                value="delete"
+              >
+                Delete contact
+              </button>
+            </li>
+          </>
+        );
+      })}
+    </ul>
+  );
+}
+
+ContactList.propTypes = {
+  contacts: PropTypes.array.isRequired,
+  deleteContact: PropTypes.func.isRequired,
+};
